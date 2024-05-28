ca0f73fdfb8a05b87bc18527ef92dbfea71acea3
Dhiogo Santos <dhiogo.santos70@gmail.com>
2024-05-28 01:38:32 -0300

    Código java contendo a solução para o desafio proposto

---
+++ src/ContaTerminal.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+/**
+ * @author Dhiogo Santos
+ * @version 1.0
+ * @since 28/05/2024
+ */
+
+public class ContaTerminal {
+
+    public static void main(String[] args) {
+
+        int numero;
+        String agencia, nomeCliente;
+        float saldo;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da conta: ");
+        numero = sc.nextInt();
+
+        sc.nextLine();
+
+        System.out.println("Por favor, digite o número da agência: ");
+        agencia = sc.nextLine();
+
+        System.out.println("Por favor, digite seu nome: ");
+        nomeCliente = sc.nextLine();
+
+        System.out.println("Por favor, digite seu saldo: R$ ");
+        saldo = sc.nextFloat();
+
+        System.out.println("\nOlá " + nomeCliente + ", "
+                            + "obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", "
+                            + "conta " + numero + " e seu saldo R$ " + saldo
+                            + " já está disponível para saque.\n");
+    }
+}
